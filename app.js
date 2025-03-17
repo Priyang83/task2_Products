f9119df4441f5258935ca90790505f74419348f5
Priyang83 <patelpriyang83@gmail.com>
2025-03-18 00:48:44 +0530

    commit

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+const cartArray=[];
 products.sort(function sortName(a, b) {
     if (a.title < b.title) {
         return -1;
     }
     else return 1;
 });
-
+const cart_Product=[];
 function displayProduct(p) {
     const temp_Array = p.map(function (val) {
         return ` <article class="product">
@@ -16,6 +17,7 @@ function displayProduct(p) {
               <footer>
                 <h5 class="product-name">${val.title}</h5>
                 <span class="product-price">${val.price}</span>
+                <button onclick="addCart('${val.id}')">Add To Cart</button>
               </footer>
             </article> ` ;
     });
@@ -23,10 +25,19 @@ function displayProduct(p) {
     document.getElementById("products").innerHTML = temp_Array.join("");
 }
 
-const company_Array = products.map(function (val) {
+const companies=products.reduce(function(pre,val){
+    if(pre.includes(val.company)==false)
+    {
+        pre.push(val.company);
+    }
+    return pre;
+},[]);
+companies.unshift("All");
+const company_Array = companies.map(function (val) {
+    
     return ` <button class="company-btn"
-    onClick="companyFilter('${val.company}') "
-    >${val.company}
+    onClick="companyFilter('${val}') "
+    >${val}
     </button> `;
 });
 
@@ -34,7 +45,7 @@ document.getElementById("companies").innerHTML = company_Array.join("");
 
 function companyFilter(c) {
     const temp = products.filter(function myFilter(val) {
-        return c == val.company;
+        return c == val.company||c=="All";
     });
     displayProduct(temp);
 }
@@ -51,4 +62,28 @@ function sortdata(s) {
     displayProduct(products);
 }
 
+function addCart(i){
+    const item=cartArray.find(function(val){
+        return val.id== i;
+    });
+    if(item){
+        item.qty+=1;
+    }
+    else{
+        const newItem=products.find(function(val){
+            return val.id== i;
+        });
+        newItem.qty=1;
+        cartArray.push(newItem);
+    }
+    const cartCount=cartArray.length;
+    document.getElementById("cart_count").innerHTML=cartCount;
+}
+
+function checkout(){
+    const chkout=cartArray.reduce(function(pre,val){
+        return pre + val.qty * val.price;
+    },0);
+    document.getElementById("checkout_val").innerHTML=chkout;
+}
 displayProduct(products);
